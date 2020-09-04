859e7fb4eb0d6b24c02091784a5f9b53819ba413
Ieuan <HortonIeuan@gmail.com>
2020-09-04 11:44:35 -0400

    bop

--- credentials.py
+++ credentials.py
@@ -1,4 +1,7 @@
 from Robinhood import Robinhood
-
-Robinhood_client = Robinhood()
-Robinhood_client.login(username='',password='')
+my_trader = Robinhood()
+logged_in = my_trader.login(username="USERNAME HERE", password="PASSWORD HERE")
+stock_instrument = my_trader.instruments("GEVO")[0]
+quote_info = my_trader.quote_data("GEVO")
+buy_order = my_trader.place_market_buy_order(stock_instrument["url"], "GEVO", "GFD", 1)
+sell_order = my_trader.place_market_sell_order(stock_instrument["url"], "GEVO", "GFD", 1)
